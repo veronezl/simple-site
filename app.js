18e8d9121c2c730b7dd5498ab775522ba3c183b6
lucasveronez <lucasveronez.dev@gmail.com>
2024-12-09 21:10:25 -0300

    feat: adding event click to button

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict'
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Escuro";
+    } else {
+        this.textContent = "Claro";
+    }
+
+    console.log('current class name: ' + className);
+});
